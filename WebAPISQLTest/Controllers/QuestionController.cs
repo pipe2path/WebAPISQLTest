dd8e67e44f54c54021cdfe42db3c2cb9abe36452
Kevin Menezes <kevinmenezes@yahoo.com>
2019-10-28 20:25:48 -0700

    controller added

---
+++ WebAPISQLTest/Controllers/QuestionController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPISQLTest.Interfaces;
+using WebAPISQLTest.Models;
+
+namespace WebAPISQLTest.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private IQuestionRepository _questionRepository;
+
+        public QuestionController(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+
+        [Route("api/questions")]
+        public async Task<List<Question>> Get()
+        {
+            return await _questionRepository.GetQuestions();
+        }
+
+        [Route("api/question")]
+        public async Task<Question> GetQuestionById(int id)
+        {
+            if (id <= 0)
+                return null;
+            else
+            {
+                return await _questionRepository.GetQuestionById(id);
+            }
+        }
+    }
+}
